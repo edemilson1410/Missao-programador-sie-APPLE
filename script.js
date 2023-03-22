3716ebc75b29fce99367dd7dbc06a35138316293
edemilson1410 <83887233+edemilson1410@users.noreply.github.com>
2023-03-22 12:08:52 -0300

    Atualização
    Foi atualizado o código, acrescentado nova função nos botões.

---
+++ script.js
@@ -0,0 +1,8 @@
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    circulo.style.background = cor
+}
+function trocaImg(imagem) {
+    let iphone = document.querySelector(".iphone")
+    iphone.src = imagem
+}
